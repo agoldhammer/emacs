f0e960837a4c77ddc1f88779a12356d530c33c8a
Art Goldhammer <art.goldhammer@gmail.com>
2017-08-22 19:13:27 -0400

    more cleanup

--- init.el
+++ init.el
@@ -15,6 +15,9 @@
   ;;	       '("marmalade" . "http://marmalade-repo.org/packages/") t)
   (package-initialize))
 
+;; needed for gterm over ssh
+(xterm-mouse-mode t)
+
 (eval-when-compile (require 'use-package))
 (setq use-package-verbose t)
 (setq use-package-always-ensure t)
@@ -335,10 +338,11 @@
     (turn-on-haskell-indent turn-on-font-lock turn-on-eldoc-mode turn-on-haskell-doc-mode turn-on-haskell-unicode-input-method)))
  '(package-selected-packages
    (quote
-    (counsel ivy-smex sphinx-doc dot-mode neotree company-quickhelp cycle-resize kibit-helper realgud hydra ag jquery-doc flymake-jslint web-mode ace-window smartparens-config zencoding zencoding-mode which-key use-package unbound swiper smex smartparens ranger rainbow-mode rainbow-delimiters projectile org multiple-cursors magit key-chord js2-mode idomenu ido-yes-or-no ido-vertical-mode ido-select-window ido-grid-mode ido-exit-target ido-describe-bindings git-gutter flycheck flx-ido fill-column-indicator eyebrowse expand-region exec-path-from-shell emmet-mode elpy cycbuf company-jedi column-enforce-mode clojure-snippets cider avy-zap anaconda-mode)))
+    (counsel ivy-smex sphinx-doc dot-mode neotree company-quickhelp cycle-resize kibit-helper realgud hydra ag jquery-doc flymake-jslint web-mode ace-window smartparens-config zencoding zencoding-mode which-key use-package swiper smex smartparens ranger rainbow-mode rainbow-delimiters projectile org multiple-cursors magit js2-mode idomenu ido-yes-or-no ido-vertical-mode ido-select-window ido-grid-mode ido-exit-target ido-describe-bindings git-gutter flycheck flx-ido fill-column-indicator eyebrowse expand-region exec-path-from-shell emmet-mode elpy cycbuf company-jedi column-enforce-mode clojure-snippets cider avy-zap anaconda-mode)))
  '(save-place-mode t nil (saveplace))
  '(show-paren-mode t)
- '(winner-mode t))
+ '(winner-mode t)
+ '(xterm-mouse-mode t))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -569,96 +573,96 @@ See also `newline-and-indent'."
 ;;             ("k" sp-kill-sexp "Kill" :color blue)
 ;;             ("q" nil "Quit" :color blue))
 ;;           smartparens-mode-map)
-(bind-key "H-s"
-	  (defhydra hy-smartparens (:hint nil)
-	    "
-  _B_ backward-sexp            -----
-  _F_ forward-sexp               _s_ splice-sexp
-  _L_ backward-down-sexp         _df_ splice-sexp-killing-forward
-  _H_ backward-up-sexp           _db_ splice-sexp-killing-backward
-^^------                         _da_ splice-sexp-killing-around
-  _k_ down-sexp                -----
-  _j_ up-sexp                    _C-s_ select-next-thing-exchange
--^^-----                         _C-p_ select-previous-thing
-  _n_ next-sexp                  _C-n_ select-next-thing
-  _p_ previous-sexp            -----
-  _a_ beginning-of-sexp          _C-f_ forward-symbol
-  _z_ end-of-sexp                _C-b_ backward-symbol
---^^-                          -----
-  _t_ transpose-sexp             _c_ convolute-sexp
--^^--                            _g_ absorb-sexp
-  _x_ delete-char                _e_ emit-sexp
-  _dw_ kill-word               -----
-  _dd_ kill-sexp                 _,b_ extract-before-sexp
--^^--                            _,a_ extract-after-sexp
-  _S_ unwrap-sexp              -----
--^^--                            _AP_ add-to-previous-sexp
-  _C-h_ forward-slurp-sexp       _AN_ add-to-next-sexp
-  _C-l_ forward-barf-sexp      -----
-  _C-S-h_ backward-slurp-sexp    _ join-sexp
-  _C-S-l_ backward-barf-sexp     _|_ split-sexp
-"
-	    ;; TODO: Use () and [] - + * | <space>
-	    ("B" sp-backward-sexp );; similiar to VIM b
-	    ("F" sp-forward-sexp );; similar to VIM f
-	    ;;
-	    ("L" sp-backward-down-sexp )
-	    ("H" sp-backward-up-sexp )
-	    ;;
-	    ("k" sp-down-sexp ) ; root - towards the root
-	    ("j" sp-up-sexp )
-	    ;;
-	    ("n" sp-next-sexp )
-	    ("p" sp-previous-sexp )
-	    ;; a..z
-	    ("a" sp-beginning-of-sexp )
-	    ("z" sp-end-of-sexp )
-	    ;;
-	    ("t" sp-transpose-sexp )
-	    ;;
-	    ("x" sp-delete-char )
-	    ("dw" sp-kill-word )
-	    ;;("ds" sp-kill-symbol ) ;; Prefer kill-sexp
-	    ("dd" sp-kill-sexp )
-	    ;;("yy" sp-copy-sexp ) ;; Don't like it. Pref visual selection
-	    ;;
-	    ("S" sp-unwrap-sexp ) ;; Strip!
-	    ;;("wh" sp-backward-unwrap-sexp ) ;; Too similar to above
-	    ;;
-	    ("C-h" sp-forward-slurp-sexp )
-	    ("C-l" sp-forward-barf-sexp )
-	    ("C-S-h" sp-backward-slurp-sexp )
-	    ("C-S-l" sp-backward-barf-sexp )
-	    ;;
-	    ;;("C-[" (bind (sp-wrap-with-pair "[")) ) ;;FIXME
-	    ;;("C-(" (bind (sp-wrap-with-pair "(")) )
-	    ;;
-	    ("s" sp-splice-sexp )
-	    ("df" sp-splice-sexp-killing-forward )
-	    ("db" sp-splice-sexp-killing-backward )
-	    ("da" sp-splice-sexp-killing-around )
-	    ;;
-	    ("C-s" sp-select-next-thing-exchange )
-	    ("C-p" sp-select-previous-thing )
-	    ("C-n" sp-select-next-thing )
-	    ;;
-	    ("C-f" sp-forward-symbol )
-	    ("C-b" sp-backward-symbol )
-	    ;;
-	    ;;("C-t" sp-prefix-tag-object)
-	    ;;("H-p" sp-prefix-pair-object)
-	    ("c" sp-convolute-sexp )
-	    ("g" sp-absorb-sexp )
-	    ("e" sp-emit-sexp )
-	    ;;
-	    (",b" sp-extract-before-sexp )
-	    (",a" sp-extract-after-sexp )
-	    ;;
-	    ("AP" sp-add-to-previous-sexp );; Difference to slurp?
-	    ("AN" sp-add-to-next-sexp )
-	    ;;
-	    ("_" sp-join-sexp ) ;;Good
-	    ("|" sp-split-sexp )))
+;; (bind-key "H-s"
+;;	  (defhydra hy-smartparens (:hint nil)
+;;	    "
+;;   _B_ backward-sexp            -----
+;;   _F_ forward-sexp               _s_ splice-sexp
+;;   _L_ backward-down-sexp         _df_ splice-sexp-killing-forward
+;;   _H_ backward-up-sexp           _db_ splice-sexp-killing-backward
+;; ^^------                         _da_ splice-sexp-killing-around
+;;   _k_ down-sexp                -----
+;;   _j_ up-sexp                    _C-s_ select-next-thing-exchange
+;; -^^-----                         _C-p_ select-previous-thing
+;;   _n_ next-sexp                  _C-n_ select-next-thing
+;;   _p_ previous-sexp            -----
+;;   _a_ beginning-of-sexp          _C-f_ forward-symbol
+;;   _z_ end-of-sexp                _C-b_ backward-symbol
+;; --^^-                          -----
+;;   _t_ transpose-sexp             _c_ convolute-sexp
+;; -^^--                            _g_ absorb-sexp
+;;   _x_ delete-char                _e_ emit-sexp
+;;   _dw_ kill-word               -----
+;;   _dd_ kill-sexp                 _,b_ extract-before-sexp
+;; -^^--                            _,a_ extract-after-sexp
+;;   _S_ unwrap-sexp              -----
+;; -^^--                            _AP_ add-to-previous-sexp
+;;   _C-h_ forward-slurp-sexp       _AN_ add-to-next-sexp
+;;   _C-l_ forward-barf-sexp      -----
+;;   _C-S-h_ backward-slurp-sexp    _ join-sexp
+;;   _C-S-l_ backward-barf-sexp     _|_ split-sexp
+;; "
+;;	    ;; TODO: Use () and [] - + * | <space>
+;;	    ("B" sp-backward-sexp );; similiar to VIM b
+;;	    ("F" sp-forward-sexp );; similar to VIM f
+;;	    ;;
+;;	    ("L" sp-backward-down-sexp )
+;;	    ("H" sp-backward-up-sexp )
+;;	    ;;
+;;	    ("k" sp-down-sexp ) ; root - towards the root
+;;	    ("j" sp-up-sexp )
+;;	    ;;
+;;	    ("n" sp-next-sexp )
+;;	    ("p" sp-previous-sexp )
+;;	    ;; a..z
+;;	    ("a" sp-beginning-of-sexp )
+;;	    ("z" sp-end-of-sexp )
+;;	    ;;
+;;	    ("t" sp-transpose-sexp )
+;;	    ;;
+;;	    ("x" sp-delete-char )
+;;	    ("dw" sp-kill-word )
+;;	    ;;("ds" sp-kill-symbol ) ;; Prefer kill-sexp
+;;	    ("dd" sp-kill-sexp )
+;;	    ;;("yy" sp-copy-sexp ) ;; Don't like it. Pref visual selection
+;;	    ;;
+;;	    ("S" sp-unwrap-sexp ) ;; Strip!
+;;	    ;;("wh" sp-backward-unwrap-sexp ) ;; Too similar to above
+;;	    ;;
+;;	    ("C-h" sp-forward-slurp-sexp )
+;;	    ("C-l" sp-forward-barf-sexp )
+;;	    ("C-S-h" sp-backward-slurp-sexp )
+;;	    ("C-S-l" sp-backward-barf-sexp )
+;;	    ;;
+;;	    ;;("C-[" (bind (sp-wrap-with-pair "[")) ) ;;FIXME
+;;	    ;;("C-(" (bind (sp-wrap-with-pair "(")) )
+;;	    ;;
+;;	    ("s" sp-splice-sexp )
+;;	    ("df" sp-splice-sexp-killing-forward )
+;;	    ("db" sp-splice-sexp-killing-backward )
+;;	    ("da" sp-splice-sexp-killing-around )
+;;	    ;;
+;;	    ("C-s" sp-select-next-thing-exchange )
+;;	    ("C-p" sp-select-previous-thing )
+;;	    ("C-n" sp-select-next-thing )
+;;	    ;;
+;;	    ("C-f" sp-forward-symbol )
+;;	    ("C-b" sp-backward-symbol )
+;;	    ;;
+;;	    ;;("C-t" sp-prefix-tag-object)
+;;	    ;;("H-p" sp-prefix-pair-object)
+;;	    ("c" sp-convolute-sexp )
+;;	    ("g" sp-absorb-sexp )
+;;	    ("e" sp-emit-sexp )
+;;	    ;;
+;;	    (",b" sp-extract-before-sexp )
+;;	    (",a" sp-extract-after-sexp )
+;;	    ;;
+;;	    ("AP" sp-add-to-previous-sexp );; Difference to slurp?
+;;	    ("AN" sp-add-to-next-sexp )
+;;	    ;;
+;;	    ("_" sp-join-sexp ) ;;Good
+;;	    ("|" sp-split-sexp )))
 
 ;; (define-key f12-map (kbd "s") 'hy-smartparens/body)
 
