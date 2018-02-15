2fb77a1d9caa1639a1c940422cbac0c6ca5a0e8b
Art Goldhammer <art.goldhammer@gmail.com>
2018-02-15 08:50:19 -0500

    undid color change in evil mode

--- init.el
+++ init.el
@@ -771,17 +771,17 @@ See also `newline-and-indent'."
   (add-to-list 'evil-emacs-state-modes mode))
 
 ;; change mode-line color by evil state
-(lexical-let ((default-color (cons (face-background 'mode-line)
-				    (face-foreground 'mode-line))))
-    (add-hook 'post-command-hook
-    (lambda ()
-	(let ((color (cond ((minibufferp) default-color)
-			((evil-insert-state-p) '("#e80000" . "#ffffff"))
-			((evil-emacs-state-p)  '("#444488" . "#ffffff"))
-			((buffer-modified-p)   '("#006fa0" . "#ffffff"))
-			(t default-color))))
-	(set-face-background 'mode-line (car color))
-	(set-face-foreground 'mode-line (cdr color))))))
+;; (lexical-let ((default-color (cons (face-background 'mode-line)
+;;				    (face-foreground 'mode-line))))
+;;     (add-hook 'post-command-hook
+;;     (lambda ()
+;;	(let ((color (cond ((minibufferp) default-color)
+;;			((evil-insert-state-p) '("#f08080" . "#ffffff"))
+;;			((evil-emacs-state-p)  '("#444488" . "#ffffff"))
+;;			((buffer-modified-p)   '("#006fa0" . "#ffffff"))
+;;			(t default-color))))
+;;	(set-face-background 'mode-line (car color))
+;;	(set-face-foreground 'mode-line (cdr color))))))
 
 (provide 'init)
 ;;; init.el ends here
